464a6f341847d4240c01e5ffdf65dfb913a57f9c
Sami Matero <sami.matero@siili.com>
2019-01-26 01:48:30 +0200

    Adds some base stuff for build view

---
+++ BabySimulator/Assets/Scripts/UI/BuildView/BuildView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildView : MonoBehaviour
+{
+    public GameObject CategoryGO;
+    
+    List<BuildCategory> Categories = new List<BuildCategory>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(CategoryGO != null)
+        {
+            Categories = CategoryGO.GetComponentsInChildren<BuildCategory>().ToList();
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CategoryClicked(BuildCategory caller)
+    {
+
+    }
+}
