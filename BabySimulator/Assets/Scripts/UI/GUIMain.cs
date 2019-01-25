e96e95e5abe6c78e87a7125924f834945ef60653
Sami Matero <sami.matero@siili.com>
2019-01-25 22:44:11 +0200

    few base classes for gui

---
+++ BabySimulator/Assets/Scripts/UI/GUIMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Holds references and helper functions for GUI
+public class GUIMain : Singleton<GUIMain>
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
