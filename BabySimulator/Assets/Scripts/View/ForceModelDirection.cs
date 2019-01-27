885595ebcac80acb76c1f819fde7eba57737d1e4
Seppo Tarvainen <seppo.tarvainen@siili.com>
2019-01-27 09:52:24 +0200

    Add model direction contstraint script

---
+++ BabySimulator/Assets/Scripts/View/ForceModelDirection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceModelDirection : MonoBehaviour
+{
+    float lockPosition = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(lockPosition, transform.rotation.eulerAngles.y, lockPosition);
+    }
+}
